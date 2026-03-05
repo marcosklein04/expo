3aee483dab96ecd3e6cd9de9bdf46e4777067de7
marcosklein_ <marcosklein815@gmail.com>
2026-03-05 15:34:51 -0300

    Ajusté print.js para impresión más rápida

--- static/js/print.js
+++ static/js/print.js
@@ -1,26 +1,3 @@
-/*function rawBtPrintText(text, packageName = "ru.a402d.rawbtprinter") {
-  const textEncoded = encodeURI(String(text || ""));
-  const suffix = `#Intent;scheme=rawbt;package=${packageName};end;`;
-  window.location.href = `intent:${textEncoded}${suffix}`;
-}
-
-function startPrinterRawBt() {
-  rawBtPrintText("test");
-}*/
-
-function closePrint() {
-  if (this && this.__container__ && this.__container__.parentNode) {
-    this.__container__.parentNode.removeChild(this.__container__);
-  }
-}
-
-function setPrint() {
-  this.contentWindow.__container__ = this;
-  this.contentWindow.onbeforeunload = closePrint;
-  this.contentWindow.onafterprint = closePrint;
-  this.contentWindow.focus();
-  this.contentWindow.print();
-}
 
 function printPage(sURL) {
   const hiddenFrame = document.createElement("iframe");
